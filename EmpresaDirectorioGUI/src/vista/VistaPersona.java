e2777f7d099253468f1951178e504e54b351d6e4
kellypaltin <84852114+kellypaltin@users.noreply.github.com>
2021-08-04 19:01:50 -0500

    Add files via upload

---
+++ EmpresaDirectorioGUI/src/vista/VistaPersona.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import com.sun.security.ntlm.Client;
+import controlador.ControladorPersona;
+import excepciones.ValidadorCedula;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Cliente;
+import modelo.Directivo;
+import modelo.Empleado;
+import modelo.Persona;
+
+/**
+ *
+ * @author kellypaltin
+ */
+public class VistaPersona {
+
+    public ControladorPersona controladorPersona;
+    public Scanner teclado;
+    public Class tipo; // Tipo de las clase hija que va a realizar la gestion.
+
+    public VistaPersona(Class tipo) {
+        controladorPersona = new ControladorPersona();
+        teclado = new Scanner(System.in);
+        this.tipo = tipo;
+    }
+
+    public void menu() {
+        int opcion = 0;
+        do {
+            System.out.println("1. Crear");
+            System.out.println("2. Actualizar");
+            System.out.println("3. Buscar");
+            System.out.println("4. Eliminar");
+            System.out.println("5. Listar");
+            System.out.println("6. Salir");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1:
+                    crear();
+                    break;
+                case 2:
+                    actualizar();
+                    break;
+                case 3:
+                    buscar();
+                    break;
+                case 4:
+                    eliminar();
+                    break;
+                case 5:
+                    listar();
+                    break;
+            }
+        } while (opcion < 6);
+    }
+    public void crear() {
+        System.out.println("Ingrese el nombre");
+        String nombre = teclado.next();
+        System.out.println("Ingrese el apellido");
+        String apellido = teclado.next();
+        System.out.println("Ingrese el cedula");
+        String cedula = teclado.next();
+        System.out.println("Ingrese el direccion");
+        String direccion = teclado.next();
+        if (tipo == Cliente.class) {
+            System.out.println("Ingrese el telefono");
+            String telefono = teclado.next();
+            try {
+                System.out.println("Resultado: " + controladorPersona.crearCliente(nombre, apellido, cedula, direccion, telefono));
+            } catch (ValidadorCedula ex) {
+                Logger.getLogger(VistaPersona.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (tipo == Empleado.class) {
+            System.out.println("Ingrese el sueldo");
+            double sueldo = teclado.nextDouble();
+            System.out.println("Resultado: " + controladorPersona.crearEmpleado(nombre, apellido, cedula, direccion, sueldo));
+        } else {
+            System.out.println("Ingrese el sueldo");
+            double sueldo = teclado.nextDouble();
+            System.out.println("Ingrese el categoria");
+            String categoria = teclado.next();
+            try {
+                System.out.println("Resultado: " + controladorPersona.crearDirectivo(nombre, apellido, cedula, direccion, sueldo, categoria));
+            } catch (ValidadorCedula ex) {
+                Logger.getLogger(VistaPersona.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public Persona buscar() {
+        System.out.println("Ingrese el cedula");
+        String cedula = teclado.next();
+        Persona persona = controladorPersona.buscar(cedula);
+        System.out.println(persona);
+        return persona;
+    }
+
+    public void actualizar() {
+        Persona persona = buscar();
+        if (persona != null) {
+            System.out.println("Ingrese el nombre");
+            String nombre = teclado.next();
+            System.out.println("Ingrese el apellido");
+            String apellido = teclado.next();
+            System.out.println("Ingrese el direccion");
+            String direccion = teclado.next();
+            if (tipo == Cliente.class) {
+                System.out.println("Ingrese el telefono");
+                String telefono = teclado.next();
+                System.out.println("Resultado: " + controladorPersona.actualizarCliente(nombre, apellido, persona.getCedula(), direccion, telefono));
+            } else if (tipo == Empleado.class) {
+                System.out.println("Ingrese el sueldo");
+                double sueldo = teclado.nextDouble();
+                System.out.println("Resultado: " + controladorPersona.actualizarEmpleador(nombre, apellido, persona.getCedula(), direccion, sueldo));
+            } else {
+                System.out.println("Ingrese el sueldo");
+                double sueldo = teclado.nextDouble();
+                System.out.println("Ingrese el categoria");
+                String categoria = teclado.next();
+                System.out.println("Resultado: " + controladorPersona.actualizarDirectivo(nombre, apellido, persona.getCedula(), direccion, sueldo, categoria));
+            }
+        }
+    }
+
+    public void eliminar() {
+       Persona persona = buscar();
+        if (persona != null) {
+            System.out.println("Resultado: " + controladorPersona.eliminar(persona.getCedula()));
+        }
+    }
+    public void listar() {
+        for (Object objeto : controladorPersona.getListaPersona()) {
+            if(objeto.getClass() == Empleado.class){
+                Empleado empleado = (Empleado) objeto;
+                System.out.println(empleado);
+            }else if(objeto.getClass() == Cliente.class){
+                Cliente cliente = (Cliente) objeto;
+                System.out.println(cliente);
+            }else {
+                Directivo directivo = (Directivo) objeto;
+                System.out.println(directivo);
+            }
+        }
+    }
+
+    public ControladorPersona getControladorPersona() {
+        return controladorPersona;
+    }
+
+    public void setControladorPersona(ControladorPersona controladorPersona) {
+        this.controladorPersona = controladorPersona;
+    }
+
+    public Class getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Class tipo) {
+        this.tipo = tipo;
+    }
+
+    
+
+}
